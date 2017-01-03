5aad5b2f0363b1efdcc2b15131832e840bef2cbd
jsoon <serdeemail@gmail.com>
2017-01-03 19:59:54 +0800

    Basic features completed

--- lotto.js
+++ lotto.js
@@ -25,12 +25,15 @@
         for (var i = 0; i < number; i += 1) {
             // Participant struct
             allP.push({
-                index: i,
+                id: i,
                 participant: i
             });
         }
         var chosenP = []; // chosen participants
         var leftP = allP.slice(); // shallow copy left participants, equaling to allP in the beginning
+        var bingoTemp; // the temp chosen one in the raffling
+        var curP; // current random participant
+        var prevP; // previous random participant
         var timer; // timer of the raffle
         var interval = cfg.interval || 1000; // init interval of the timer
         if (interval <= 70) {
@@ -47,7 +50,23 @@
         } else if (cfg.step === 0) {
             axisStep = 0;
         }
-        var stopFlag;
+        var clickStartFlag = false; // if start button being clicked
+        var clickStopFlag = false; // if stop button being clicked
+        // Events
+        /**
+         * processing
+         * callback
+         * @param {object} bingo - the bingo participant
+         */
+        var processing = cfg.processing || function (bingo) { }; // processing of raffle
+        /**
+         * success
+         * callback
+         * @param {object} bingo - the bingo participant
+         * @param {array} leftP - the left participants
+         */
+        var success = cfg.success || function (bingo, leftP) { }; // bingo of raffle
+
 
         /******************************************************************
          * METHODS
@@ -61,6 +80,24 @@
             return p[Math.round(Math.random() * (p.length - 1))];
         };
 
+        /**
+         * noRepeatNumGenerator
+         * Generate a no-repeat random one to the previous from the given participants
+         * @param {array} p
+         */
+        var noRepeatNumGenerator = function (p) {
+            if (p.length > 1) {
+                curP = randomNumGenerator(p);
+                while (curP === prevP) {
+                    curP = randomNumGenerator(p);
+                }
+                prevP = curP;
+                return curP;
+            } else {
+                return p[0];
+            }
+        };
+
         // Cubic-bezier function
         var cubicBezier = {
             easeIn: BezierEasing(0.42, 0, 1, 1),
@@ -68,45 +105,72 @@
             linear: BezierEasing(0, 0, 1, 1)
         };
 
+        // Pick out the bingo one
+        var bingo = function () {
+            chosenP.push(bingoTemp);
+            for (var i = 0, length = leftP.length; i < length; i += 1) {
+                if (leftP[i].id === bingoTemp.id) {
+                    leftP.splice(i, 1);
+                    success(bingoTemp, leftP);
+                    return leftP;
+                }
+            }
+        };
+
         // Reset timer, interval, acct
         var resetTimer = function () {
             clearInterval(timer); // reset timer
             axisXTemp = axisX; // reset acct
             // Clear intervalTemp and timer to completely reset the raffling
-            // For distinguish stop raffle and ending animation finish
+            // For distinguish raffling stops and ending animation finishes
             if (arguments[0] === true) {
-                intervalTemp = interval; // reset interval
                 timer = undefined;
+                intervalTemp = interval; // reset interval
+                clickStartFlag = false;
+                clickStopFlag = false;
+                bingo();
             }
         };
 
-        // Calculate interval of raffling
-        var calcInterval = function (easeType) {
-            switch (easeType) {
+        /**
+         * calcInterval
+         * Calculate interval of raffling
+         * @param {string} type - raffling type
+         */
+        var calcInterval = function (type) {
+            switch (type) {
                 case 'easeIn':
-                    // Calc intervalTemp with cubic-bezier function
-                    intervalTemp = Math.round(interval - cubicBezier.easeIn(axisXTemp) * interval);
-                    // Make sure the value of axisX and interval stay in the correct range
-                    if (axisXTemp <= axisXMax && intervalTemp >= intervalMin) {
-                        axisXTemp += axisStep;
+                    // First time to click start raffling won't calculate the intervalTemp,
+                    // so that create one time delay(which = interval) before intervalTemp changes for UE purpose
+                    if (clickStartFlag) {
+                        // Calc intervalTemp with cubic-bezier function
+                        intervalTemp = Math.round(interval - cubicBezier.easeIn(axisXTemp) * interval);
+                        // Make sure the value of axisX and interval stay in the correct range
+                        if (axisXTemp <= axisXMax && intervalTemp > intervalMin) {
+                            axisXTemp += axisStep;
+                        } else {
+                            intervalTemp = intervalMin;
+                        }
                     } else {
-                        intervalTemp = intervalMin;
+                        clickStartFlag = true;
                     }
                     return intervalTemp;
                     break;
                 case 'easeOut':
-                    // intervalTemp = Math.round(interval - cubicBezier.easeOut(axisXTemp) * interval);
-                    // intervalTemp = intervalTemp - cubicBezier.easeOut(axisXTemp) * interval;
-
-                    // First time to stop raffling won't calculate the intervalTemp
-                    if (stopFlag) {
+                    // First time to click stop raffling won't calculate the intervalTemp
+                    if (clickStopFlag) {
                         intervalTemp = Math.round(cubicBezier.easeOut(axisXTemp) * interval);
-                    }
-                    stopFlag = true;
-                    if (axisXTemp < axisXMax && intervalTemp < interval || intervalTemp === interval) {
-                        axisXTemp += axisStep / 2;
+                        if (axisXTemp <= axisXMax && intervalTemp < interval) {
+                            axisXTemp += axisStep;
+                        } else if (intervalTemp === interval) {
+                            intervalTemp = interval;
+                            axisXTemp += axisStep;
+                        } else {
+                            intervalTemp = Infinity;
+                        }
                     } else {
-                        intervalTemp = Infinity;
+                        clickStopFlag = true;
+                        axisXTemp += axisStep; // to avoid axisXTemp = 0, which will make intervalTemp = 0
                     }
                     return intervalTemp;
                     break;
@@ -116,73 +180,57 @@
             }
         };
 
-        // Raffling processing
-        var raffleEase = function (raffling, easeType) {
-            raffling(calcInterval, easeType);
-        };
-
-        var raffling = function (calcInterval, easeType) {
-            document.getElementById('result').innerHTML = randomNumGenerator(leftP).participant;
-            // console.log(document.getElementById('result').innerHTML);
-            // timer = setTimeout(function () {
-            // // Calc intervalTemp with cubic-bezier function
-            // intervalTemp = Math.round(interval - cubicBezier.easeIn(axisXTemp) * interval);
-            // // Make sure the value of axisX and interval stay in the correct range
-            // if (axisXTemp <= axisXMax && intervalTemp >= intervalMin) {
-            //     axisXTemp += axisStep;
-            // } else {
-            //     intervalTemp = intervalMin;
-            // }
+        /**
+         * raffling
+         * Raffling timer
+         * @param {string} type - raffling type
+         */
+        var raffling = function (type) {
+            intervalTemp = calcInterval(type);
             // console.log(intervalTemp);
-            //     raffling();
-            // }, intervalTemp);
 
-
-            // console.log(intervalTemp);
             timer = setTimeout(function () {
-                intervalTemp = calcInterval(easeType);
                 if (intervalTemp !== Infinity) {
-                    raffling(calcInterval, easeType);
+                    bingoTemp = noRepeatNumGenerator(leftP);
+                    processing(bingoTemp);
+                    raffling(type);
                 } else {
                     resetTimer(true);
                 }
             }, intervalTemp);
         };
 
+        /**
+         * raffleEase
+         * Raffling processing to control raffling animation
+         * @param {function} callback - raffling function
+         * @param {string} type - raffling type
+         */
+        var raffleEase = function (callback, type) {
+            callback(type);
+        };
+
         // Start raffle
         var raffleStart = function () {
-            if (timer === undefined) {
-                console.log(timer);
-                raffleEase(raffling, 'easeIn');
+            if (leftP.length) {
+                // First time to click start raffling will calculate the number
+                if (!clickStartFlag) {
+                    bingoTemp = noRepeatNumGenerator(leftP);
+                    processing(bingoTemp);
+                }
+                if (timer === undefined) {
+                    raffleEase(raffling, 'easeIn');
+                }
             }
         };
 
         // Stop raffle
         var raffleStop = function () {
-            // if (timer !== undefined) {
-            //     console.log(timer);
-            // }
-            resetTimer();
-            raffleEase(raffling, 'easeOut');
-            // var bingo = randomNumGenerator(leftP);
-            // leftP.splice(bingo.index, 1);
-            // chosenP.push(bingo);
-            // return chosenP.slice();
+            if (timer !== undefined && !clickStopFlag && intervalTemp === intervalMin) {
+                resetTimer();
+                raffleEase(raffling, 'easeOut');
+            }
         };
-
-
-
-
-
-        // for (var i = 0, sum = '', curNumber, prevNumber; i < number; i += 1) {
-        //     while (curNumber === prevNumber) {
-        //         curNumber = randomNumGenerator(number);
-        //     }
-        //     sum += curNumber + ', ';
-        //     prevNumber = curNumber;
-        // }
-        // sum = sum.slice(0, -2);
-        // document.body.insertBefore(document.createTextNode(sum), document.body.firstChild);
 
         // exports
         that.raffleStart = raffleStart;
